8e0da5a8f98018b2e28928571fe3753d064a0177
billscarp <billscarp@gmail.com>
2017-08-27 14:45:55 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,10 @@
-
-
 var ScoreToMatch;
 var yourScore;
 var wins;
 var losses;
 
 // Used Math.random to generate the number between 19 an 120
-ScoreToMatch = Math.floor(Math.random() * 101 ) + 19;
+ScoreToMatch = Math.floor(Math.random() * 101) + 19;
 console.log(ScoreToMatch);
 
 $("#random").html('Score to Match:');
@@ -14,20 +12,33 @@ $("#random").html('Score to Match:');
 
 // for loop for random number - Also adjusts number of crystals on my page
 
-for(var i = 0; i < 4; i++){
+for (var i = 0; i < 4; i++) {
 
-  // variable generating a random number for the crystals (divs)  
-    
-    var random = Math.floor(Math.random() * 12);
-        console.log (random);
+    // variable generating a random number for the crystals (divs)  
 
-  // div appeneded to html doc 
-var crystal = $("<div>");
+    var random = Math.floor(Math.random() * 12) + 1;
+    console.log(random);
+
+    // div appeneded to html doc 
+
+    var crystal = $("<div>");
     crystal.attr({
         "class": 'crystal',
-        "data-random": random
-    }); 
+        "RandomInfo": random
+    });
 
-$(".crystals").append(crystal);
+    $(".crystals").append(crystal);
 
 }
+
+// Used this to set it up so that the click feautre works on the four divs
+
+$(".crystal").on('click', function () {
+
+    console.log($(this).attr("RandomInfo"));
+
+
+
+
+
+});
